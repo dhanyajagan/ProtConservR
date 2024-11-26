5ed3d270be997c5fe21f1cfa5b787f880d78e3e6
dhanyajagannathan <dhanya.jagannathan@mail.utoronto.ca>
2024-11-26 12:04:40 -0500

    vignette fix attempt: made alignment object as data object in vignette.

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' MSA Alignment Object For Dermcidin and Lacritin Protein Sequences
+#'
+#' Dermcidin and Lacritin Protein Sequences (anti-microbial proteins) taken from InterPro
+#'
+#' @source https://www.ebi.ac.uk/interpro/entry/InterPro/IPR043557/.
+#'
+#' @format List containing:
+#' \describe{
+#'  \item{alignment}{The multiple sequence alignment object.}
+#'  \item{metadata}{List with alignment statistics.}
+#' }
+#' @examples
+#' \dontrun{
+#'  Dermcidin_Lacritin_Alignment
+#' }
+"Dermcidin_Lacritin_Alignment"
